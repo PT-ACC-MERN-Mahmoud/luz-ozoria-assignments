aaec8a7cf23c129c7012cd274b79546004d6dabf
ldozoria <97138607+ldozoria@users.noreply.github.com>
2022-07-18 01:28:57 -0400

    Add files via upload

---
+++ week6/productManagerTwo/client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './views/Main';
+import Detail from './components/Detail';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Main/>} path="/products/" />
+          <Route element={<Detail/>} path="/products/:id/" />
+        </Routes>
+      </BrowserRouter>                           
+    </div>
+  );
+}
+export default App;
+
---
+++ week6/productManagerTwo/server/controllers/product.controller.js
@@ -0,0 +1,33 @@
+const Product = require('../models/product.model');
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.createProduct = (request, response) => {
+    Product.create(request.body)
+        .then(product => response.json(product))
+        .catch(err => response.json(err));
+}
+
+module.exports.getAllProducts = (request, response) => {
+    Product.find({})
+        .then(products => {
+            console.log(products);
+            response.json(products);
+        })
+        .catch(err => {
+            console.log(err)
+            response.json(err)
+        })
+}
+
+module.exports.getOneProduct = (request, response) => {
+    Product.findOne({_id:request.params.id})
+        .then(product => response.json(product))
+        .catch(err => response.json(err))
+}
+
+
+
