fd7eb2a16fec1c04a85482126e5d7aebeb5e577d
ldozoria <97138607+ldozoria@users.noreply.github.com>
2022-07-17 23:38:40 -0400

    Add files via upload

---
+++ week5/Jokes/server/routes/jokes.route.js
@@ -0,0 +1,10 @@
+const JokeController = require('../controllers/joke.controller');
+
+module.exports = app => {
+    app.get('/api/jokes', JokeController.findAllJokes);
+    app.get('/api/jokes/:_id', JokeController.findOneJoke);
+    app.put('/api/jokes/:_id', JokeController.updateExistingJoke);
+    app.post('/api/jokes', JokeController.createNewJoke);
+    app.delete('/api/jokes/:_id', JokeController.deleteAnExistingJoke);
+
+}
